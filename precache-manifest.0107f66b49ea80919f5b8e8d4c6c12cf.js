4a363551ee5df963b7ea6b1241341cde8b9f0d73
EconClass <bizmail327@gmail.com>
2019-03-24 09:31:18 -0700

    Updates

---
+++ precache-manifest.0107f66b49ea80919f5b8e8d4c6c12cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8657d61ae275b59ef78",
+    "url": "/EconClass/poketeamreact/static/css/main.739f6f14.chunk.css"
+  },
+  {
+    "revision": "f8657d61ae275b59ef78",
+    "url": "/EconClass/poketeamreact/static/js/main.02044302.chunk.js"
+  },
+  {
+    "revision": "b2a39890f7db320c0c49",
+    "url": "/EconClass/poketeamreact/static/js/runtime~main.0e3f4154.js"
+  },
+  {
+    "revision": "15426eec75367a430d4e",
+    "url": "/EconClass/poketeamreact/static/js/2.e6e200f5.chunk.js"
+  },
+  {
+    "revision": "48cfc5134ff71f106905c8da08f88e63",
+    "url": "/EconClass/poketeamreact/index.html"
+  }
+];
